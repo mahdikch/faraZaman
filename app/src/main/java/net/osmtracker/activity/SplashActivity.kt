b2f9dc68299f5ae345c9931cfa7c6ec3454d432f
mhd.kanafchian <mahdikanafchian2212@gmail.com>
2025-05-01 17:23:09 +0330

    set them for splash

--- app/src/main/java/net/osmtracker/activity/SplashActivity.kt
+++ app/src/main/java/net/osmtracker/activity/SplashActivity.kt
@@ -1,18 +1,13 @@
 package net.osmtracker.activity
 
-import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
-import android.preference.PreferenceManager
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import net.osmtracker.OSMTracker
 import net.osmtracker.R
 
 class SplashActivity : AppCompatActivity() {
+
     companion object {
         // مدت زمان نمایش اسپلش (۳ ثانیه)
         private const val SPLASH_DELAY = 3000L
@@ -22,21 +17,9 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        // بررسی اینکه آیا Intro باید نمایش داده بشه
-        val showAppIntro = PreferenceManager.getDefaultSharedPreferences(this)
-            .getBoolean(OSMTracker.Preferences.KEY_DISPLAY_APP_INTRO, OSMTracker.Preferences.VAL_DISPLAY_APP_INTRO)
-
         // استفاده از Handler برای تأخیر
         Handler(Looper.getMainLooper()).postDelayed({
-            val intent = if (showAppIntro) {
-                // نمایش Intro اگه اولین باره
-                Intent(this, Intro::class.java)
-            } else {
-                // انتقال به TrackManager
-                Intent(this, TrackManager::class.java)
-            }
-            startActivity(intent)
-            // بستن SplashActivity
+            // بستن SplashActivity برای بازگشت به TrackManager
             finish()
         }, SPLASH_DELAY)
     }
